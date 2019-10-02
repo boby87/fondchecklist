e45c4d72864b49555d339ccc43405fb2e1dcb0d2
joeligor95 <jokayam95@gmail.com>
2019-10-02 12:57:06 +0200

    design

--- src/app/Service/AdressIp.ts
+++ src/app/Service/AdressIp.ts
@@ -1,6 +1,6 @@
 
 export class AdressIp {
-   // static host="https://checklist237.herokuapp.com/";
+    static host="https://checklist237.herokuapp.com/";
    //static host="http://192.168.1.173:8080/";
-    static host="http://localhost:8080/";
+    //static host="http://localhost:8080/";
 }
